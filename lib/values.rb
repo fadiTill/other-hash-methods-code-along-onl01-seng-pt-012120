e3264f638a4fc57fee92b426a15de5632d77754d
fadimata tillman <fadi.tillman@gmail.com>
2020-02-03 06:34:27 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -1,13 +1,22 @@
 require 'pry'
 
 #This is the groceries hash we'll be passing in to the get_the_values method
-# groceries = {
-#  dairy: ["milk", "yogurt", "cheese"],
-#  vegetable: ["carrots", "broccoli", "cucumbers"],
-#  meat: ["chicken", "steak", "salmon"],
-#  grains: ["rice", "pasta"]
-# }
+#groceries = {
+ # dairy: ["milk", "yogurt", "cheese"],
+ #  vegetable: ["carrots", "broccoli", "cucumbers"],
+ #  meat: ["chicken", "steak", "salmon"],
+ #  grains: ["rice", "pasta"]
+ # }
 
 def get_the_values(groceries)
+  groceries = {
+   dairy: ["milk", "yogurt", "cheese"],
+    vegetable: ["carrots", "broccoli", "cucumbers"],
+    meat: ["chicken", "steak", "salmon"],
+    grains: ["rice", "pasta"]
   #code your solution here!
 end
+
+ groceries.keys
+
+ 
