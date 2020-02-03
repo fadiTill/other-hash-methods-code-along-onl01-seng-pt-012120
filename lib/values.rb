2acd9c887105722325337704064e12ae21384340
fadimata tillman <fadi.tillman@gmail.com>
2020-02-03 06:43:29 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -15,5 +15,5 @@ def get_the_values(groceries)
     meat: ["chicken", "steak", "salmon"],
     grains: ["rice", "pasta"]
 }
-  groceries.values#code your solution here!
+  groceries.values.flatten#code your solution here!
 end
